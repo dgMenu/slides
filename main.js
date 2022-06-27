03f075b1ea42da7ecf86a09930f671fc93d2e61d
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-06-27 15:34:47 +0530

    viva

---
+++ main.js
@@ -0,0 +1,17 @@
+import "reveal.js/dist/reset.css";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/plugin/highlight/night-owl.css";
+import "./brand.css";
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+
+import Notes from "reveal.js/plugin/notes/notes.esm.js";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
+import Search from "reveal.js/plugin/search/search.esm.js";
+import Zoom from "reveal.js/plugin/zoom/zoom.esm.js";
+
+let deck = new Reveal({
+  plugins: [Markdown, Notes, Search, Zoom, RevealHighlight],
+  hash: true,
+});
+deck.initialize();
